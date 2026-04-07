9b5fbb6476d0a5b94ccaaa1576c4d8023accb7f3
Dish Khant <dish.khant.dev@gmail.com>
2026-04-08 05:10:35 +0530

    Featured Brands Responsive.

--- src/components/pages/home/FeaturedBrands.tsx
+++ src/components/pages/home/FeaturedBrands.tsx
@@ -7,9 +7,9 @@ import Image from "next/image";
 import VersaceLogo from "@/assets/images/pages/home/featured-brands/versace.png";
 const FeaturedBrandsSection = () => {
     return (
-        <div className="bg-[#000000] padding-main-x flex items-center gap-[106px] py-10.5">
+        <div className="bg-[#000000] padding-main-x flex flex-wrap items-center justify-center xl:justify-start gap-8.5 sm:gap-16 lg:gap-16 xl:gap-[106px] py-10.5">
             {[1, 2, 3, 4, 5].map((item) => (
-                <div key={item} className="relative w-40 h-9">
+                <div key={item} className="relative w-24 h-6 sm:w-32 sm:h-8 md:w-40 md:h-9">
                     <Image
                         src={VersaceLogo}
                         alt="Brand Logo"
